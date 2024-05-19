5b5819b9a43c18918ad6f38b470330eb2155ef83
gilbertugo <gilberteyiuche@gmail.com>
2024-05-19 14:52:01 +0100

    fine

--- yes/ugo.js
+++ yes/ugo.js
@@ -1,27 +1,27 @@
-setInterval(app, 1000);
+setInterval(showTime, 1000);
 
-function app(){
+function showTime(){
     var date = new Date();
-    var hr = date.getHours();
-    var mn = date.getMinutes();
-    var sec = date.getSeconds
-     var session = "AM";
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var session = "AM";
       
-      if(hr == 0){
-        hr < 12
+      if(h == 0){
+        h = 12
       }
       
-      if(hr > 12){
-        hr = hr - 12;
-        session = "PM"
+      if(h > 12){
+        h = h - 12;
+        session = "PM";
       }
       
-      hr = (hr < 10)? " 0 " + hr:hr;
-      mn = (mn < 10)? " 0 " + mn:mn;
-      sec = (sec < 10)? " 0 " + sec:sec;
+      h = (h < 10)? "0" + h:h;
+      m = (m < 10)? "0" + m:m;
+      s = (s < 10)? "0" + s:s;
 m
-      var time = hr + ":" + mn + ":" + sec + "" + session;
+      var time = h + ":" + m + ":" + s + "" + session;
       document.getElementById('clock').innerHTML = time;
 
 }
-app();
+
